9a5aa1ca73d2cd9d7204c806fda7a4fc12318976
Lukasz Borowczak <lborowczak@luc.edu>
2016-03-13 16:49:58 -0500

    Switch to using web fonts, add counter of how many letters there have been in total, and update progress bar

--- game.js
+++ game.js
@@ -4,8 +4,8 @@ var ctx = canvas.getContext("2d");
 canvas.style.backgroundColor = "orange"
 
 //Global variable setup
-var counter = 0;
-var sync = 0;
+var correctLettersCounter = 0;
+var totalLettersCounter = -1;
 var velocity = -5;
 var topOffset = 20;
 var letterCanvasWidth=28;
@@ -13,6 +13,7 @@ var letters = new Array();
 var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var offsets = new Array();
 setupOffsets();
+var progressBar = document.getElementById("progressBar");
 
 //add a canvas for each character
 var strokeFonts = new Array();
@@ -39,7 +40,6 @@ function render() {
  drawGame();
  requestAnimationFrame(render);
  lastRender = Date.now();
- sync = 1;
 }
 
 function drawGame() {
@@ -53,44 +53,52 @@ function setupOffsets() {
 }
 
 function addCanvases() {
-	 ctx.textBaseline = "hanging";
-	 ctx.font = "38px arial";
-	 ctx.textAlign = "left";
- for (var i=0; i < 27; i++) {
-	 var renderedText = document.createElement('canvas');
-	 renderedText.width = letterCanvasWidth;
-	 renderedText.height = 40;
-	 var tmpCtx = renderedText.getContext('2d');
-	 tmpCtx.textBaseline = "hanging";
-	 tmpCtx.font = "38px monospace";
-	 tmpCtx.textAlign = "left";
-	 tmpCtx.strokeText(alphabet[i], 2, 2);
-	 tmpCtx.strokeText(alphabet[i], 2, 2);
-	 tmpCtx.strokeText(alphabet[i], 2, 2);
-	 tmpCtx.strokeText(alphabet[i], 2, 2);
-	 tmpCtx.strokeText(alphabet[i], 2, 2);
-	 tmpCtx.strokeText(alphabet[i], 2, 2);
-	 tmpCtx.strokeText(alphabet[i], 2, 2);
-	 tmpCtx.strokeText(alphabet[i], 2, 2);
-	 strokeFonts.push(renderedText);
- }
- for (var i=0; i < 27; i++) {
-	 var renderedText = document.createElement('canvas');
-	 renderedText.width = letterCanvasWidth;
-	 renderedText.height = 40;
-	 var tmpCtx = renderedText.getContext('2d');
-	 tmpCtx.textBaseline = "hanging";
-	 tmpCtx.font = "38px monospace";
-	 tmpCtx.textAlign = "left";
-	 tmpCtx.fillText(alphabet[i], 2, 2);
-	 fillFonts.push(renderedText);
- }
+    ctx.textBaseline = "hanging";
+    ctx.font = "38px Droid Sans Mono";
+    ctx.textAlign = "left";
+
+    for (var i=0; i < 27; i++) {
+        var renderedText = document.createElement('canvas');
+        renderedText.width = letterCanvasWidth;
+        renderedText.height = 40;
+        var tmpCtx = renderedText.getContext('2d');
+        tmpCtx.textBaseline = "hanging";
+        tmpCtx.font = "38px Droid Sans Mono";
+        tmpCtx.textAlign = "left";
+        tmpCtx.strokeText(alphabet[i], 2, 2);
+        tmpCtx.strokeText(alphabet[i], 2, 2);
+        tmpCtx.strokeText(alphabet[i], 2, 2);
+        tmpCtx.strokeText(alphabet[i], 2, 2);
+        tmpCtx.strokeText(alphabet[i], 2, 2);
+        tmpCtx.strokeText(alphabet[i], 2, 2);
+        tmpCtx.strokeText(alphabet[i], 2, 2);
+        tmpCtx.strokeText(alphabet[i], 2, 2);
+        strokeFonts.push(renderedText);
+    }
+
+    for (var i=0; i < 27; i++) {
+        var renderedText = document.createElement('canvas');
+        renderedText.width = letterCanvasWidth;
+        renderedText.height = 40;
+        var tmpCtx = renderedText.getContext('2d');
+        tmpCtx.textBaseline = "hanging";
+        tmpCtx.font = "38px Droid Sans Mono";
+        tmpCtx.textAlign = "left";
+        tmpCtx.fillText(alphabet[i], 2, 2);
+        fillFonts.push(renderedText);
+    }
 }
 
 function drawTopLetters() {
- for (var i=0; i < alphabet.length; i++) {
- ctx.drawImage(strokeFonts[i], offsets[i], topOffset);
- }
+    for (var i=0; i < alphabet.length; i++) {
+    ctx.drawImage(strokeFonts[i], offsets[i], topOffset);
+    }
+}
+
+function updateCounter() {
+    progressBar.setAttribute("value", correctLettersCounter);
+    progressBar.setAttribute("max", totalLettersCounter);
+    document.getElementById('counter').innerHTML = correctLettersCounter +"/" + totalLettersCounter;
 }
 
 function drawUpcomingLetters() {
@@ -122,6 +130,8 @@ function deleteOldLetters() {
  if (letters.length == 0) return;
  if (!(letters[0][1] > -100)) {
 	 letters.shift();
+	 totalLettersCounter += 1;
+	 updateCounter();
  }
 }
 
@@ -141,8 +151,7 @@ function doKeyDown(e) {
 
 		 var letterLoc = letters[i][1];
 		 if (letterLoc < 45 && letterLoc > -45) {
-		 	 counter +=1;
-         	 document.getElementById('counter').innerHTML = counter;
+		 	 correctLettersCounter +=1;
 			 clearLetter(letters[i]);
 			 drawLetter("stroke", key, topOffset);
 			 letters[i][1] = -200;
