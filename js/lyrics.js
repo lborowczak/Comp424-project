36933cba80813097395e905b88a93218fc3f417a
Carl Reider <creider@luc.edu>
2016-04-18 21:20:38 -0500

    Trying to get .GETJSON working

--- js/lyrics.js
+++ js/lyrics.js
@@ -1,21 +1,34 @@
+$.getJSON({
+
+  data: {
+        apikey:"85d2e353820e89033afa96e3391ba32d",
+        q_artist: "Lose Yourself",
+        format:"jsonp",
+        callback:"jsonp_callback"
+    },
+    url: "http://api.musixmatch.com/ws/1.1/track.search",
+    dataType: "jsonp",
+    jsonpCallback: 'jsonp_callback',
+    contentType: 'application/json',
+    success: function(data) {
+        console.log(data.message.body);
+    },
+
+  });
+
+/*
 var music = require('musicmatch')({guid:"85d2e353820e89033afa96e3391ba32d"});
-if(music){
-var log = console.log("Success")
-console.log(music)
-}
-
-
-
-
-
-music.trackSearch({q:"Lukas Graham" ,page:1,page_size:3})
+if(music){var log = console.log("Success")}
+music.trackSearch({q:"Jay-Z" ,page:1,page_size:10})
 .then(function(data){
-  var songName = data.message.body.track_list[0].track.track_id
+  var songName = data.message.body.track_list[8].track.track_id
 music.trackLyrics({track_id:songName}).then(function(data){
   var lyrics = data.message.body.lyrics.lyrics_body
   var result = lyrics.split('\n')
-  console.log(result[0])
+  for(var i = 0, len = result.length; i <len; i++)
+      console.log(result[i])
   })
 }).catch(function(err){
    console.log(err);
 })
+*/
